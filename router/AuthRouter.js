2ec34f09652871da0b41abfe59d789e274a2b89c
Mashrur Mahmud Hossain <mahmudmashrur0@gmail.com>
2026-07-16 17:18:14 +0600

    commit done

--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -21,8 +21,20 @@ router.post('/register', async(req, res) => {
     res.status(201).json({user, success: true, message: "User created successfully"})
 })
 
-router.post('/login', (req, res) => {
-    res.send('login')
+router.post('/login', async(req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({message: "All fields are required", success: false})
+    }
+    const exist = await User.findOne({email})
+    if(!exist){
+        return res.status(400).json({message: "User does not exist", success: false})
+    }
+    if(exist.password !== password){
+        return res.status(400).json({message: "Incorrect password", success: false})
+    }
+    res.status(200).json({user: exist, success: true, message: "User logged in successfully"})
+
 })
 
 export default router
