dadd43e8ae61e3f1ee8a5ff4865ce875332cdbd4
Mashrur Mahmud Hossain <mahmudmashrur0@gmail.com>
2026-07-16 11:25:47 +0600

    some change

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const port = process.env.PORT
 const startServer= async()=>{
     try{
         await connectDB();
-        app.listen(port, ()=>{
-            console.log(`Server is running on port ${port}`)
-        })
+        // app.listen(port, ()=>{
+        //     console.log(`Server is running on port ${port}`)
+        // })
 
     }catch(error){
         console.log(error, "fail to load server")
